3a95b302b7ee8979cf9d8dda5a33256cd754ce1d
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-05-25 16:04:27 +1000

    Update model_1.py

--- model_1.py
+++ model_1.py
@@ -1,15 +1,19 @@
+# Import libraries
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import tensorflow_datasets as tfds
 
+# Create the data
 (train_data, test_data), info = tfds.load('imdb_reviews/subwords8k',
                                     split=(tfds.Split.TRAIN, tfds.Split.TEST),
                                     with_info=True, as_supervised=True)
 
 
+# Define the encoder
 encoder = info.features['text'].encoder
 
+# Define the training and testing batches
 padded_shapes = ([None], ())
 train_batches = train_data.shuffle(1000).padded_batch(10,
                                             padded_shapes=padded_shapes)
@@ -17,6 +21,7 @@ train_batches = train_data.shuffle(1000).padded_batch(10,
 test_batches = test_data.shuffle(1000).padded_batch(10,
                                             padded_shapes=padded_shapes)
 
+# Create the model
 embedding_dim = 16
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(encoder.vocab_size, embedding_dim),
@@ -24,9 +29,11 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Compile the model
 model.compile(loss='binary_crossentropy',
             optimizer='Adam',
             metrics=['accuracy'])
 
+# Fit the model
 history = model.fit(train_batches, epochs=10, validation_data=(test_batches),
                     validation_steps=20)
